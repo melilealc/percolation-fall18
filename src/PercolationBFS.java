af3b0767ccdf5c4daee8398991662efbc2300ac2
melilealc <mcl52@duke.edu>
2018-11-29 17:57:26 -0500

    BFS and comments

--- src/PercolationBFS.java
+++ src/PercolationBFS.java
@@ -12,29 +12,24 @@ public class PercolationBFS extends PercolationDFSFast{
 	 * 
 	 */
 	public void dfs(int row, int col) {
-		int size = myGrid.length; 
-	
-	        int[] rowDelta = {-1,1,0,0};
-	        int[] colDelta = {0,0,-1,1};
-	        
-	       // if (myGrid[row][col] != lookFor) return 0; // not part of blob
-	        
-	        Queue<Integer> queuePerc = new LinkedList<Integer>();       
-	        //myGrid[row][col] = fillWith;  // mark pixel
-	                          // count pixel
-	        queuePerc.add(row*size + col );
-	        while (queuePerc.size() != 0){
-	            int value = queuePerc.remove();
-	            for(int k=0; k < rowDelta.length; k++){
-	                row = value/size + rowDelta[k];
-	                col = value%size + colDelta[k];
-	                if (inBounds(row,col) && isOpen(row, col) && !isFull(row, col)){
-	                	queuePerc.add(row*size + col);
-	                     myGrid [row][col]= FULL;
+		int size = myGrid.length;
+	    int[] rowDelta = {-1,1,0,0};
+	    int[] colDelta = {0,0,-1,1};
+	    Queue<Integer> queuePerc = new LinkedList<Integer>();     
+	    myGrid [row][col] = FULL; 
+	    queuePerc.add(row*size + col );
+	    while (queuePerc.size() != 0){
+	    	int value = queuePerc.remove();
+	    	for(int k=0; k < rowDelta.length; k++){
+	    		row = value/size + rowDelta[k];
+	            col = value%size + colDelta[k];
+	            if (inBounds(row,col) && isOpen(row, col) && ! isFull(row, col)){
+	            	queuePerc.add(row*size + col);
+	                myGrid [row][col]= FULL;
 	                    
-	                	}
-	            	}
-	        	}
+	               	}
+	            }
+	        }
 		}
 }
 
--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -1,6 +1,6 @@
 
 public class PercolationDFSFast extends PercolationDFS{
-
+ 
 	public PercolationDFSFast(int size) {
 		super(size);
 		
